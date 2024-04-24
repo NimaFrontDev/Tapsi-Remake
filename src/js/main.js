19ff89223b8a2d9df685bb7865943ef069280ee6
Nima TorkZad <nimatorkzad@gmail.com>
2024-04-24 19:14:15 +0330

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+// Hamburger Menu :
+
+const toggler = document.querySelector(".nav__toggler");
+const navbar = document.querySelector(".nav");
+toggler.addEventListener("click", (e) => {
+  navbar.classList.toggle("nav__expanded");
+});
+
+
+// Tapsi Services : 
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    const targetTabContent = document.querySelector(
+      `#${tab.dataset.tabTarget}`
+    );
+
+    //Remove Previous Activated Classes
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
+
+    //Add New Active Classes
+    tab.classList.add("active");
+    targetTabContent.classList.add("active");
+  });
+});
