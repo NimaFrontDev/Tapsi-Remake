35c43e6072371b04de45837d1c9bcb996ce55b90
Nima TorkZad <nimatorkzad@gmail.com>
2024-04-27 18:31:16 +0330

    accordion styling

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,33 @@
-// Hamburger Menu :
+// // Hamburger Menu :
 
-const toggler = document.querySelector(".nav__toggler");
-const navbar = document.querySelector(".nav");
-toggler.addEventListener("click", (e) => {
-  navbar.classList.toggle("nav__expanded");
-});
+// const toggler = document.querySelector(".nav__toggler");
+// const navbar = document.querySelector(".nav");
+// toggler.addEventListener("click", (e) => {
+//   navbar.classList.toggle("nav__expanded");
+// });
 
+// // Tapsi Services :
+// const tabs = document.querySelectorAll(".tab");
+// const tabContents = document.querySelectorAll(".tab-content");
+// tabs.forEach((tab) => {
+//   tab.addEventListener("click", (e) => {
+//     const targetTabContent = document.querySelector(
+//       `#${tab.dataset.tabTarget}`
+//     );
 
-// Tapsi Services : 
-const tabs = document.querySelectorAll(".tab");
-const tabContents = document.querySelectorAll(".tab-content");
-tabs.forEach((tab) => {
-  tab.addEventListener("click", (e) => {
-    const targetTabContent = document.querySelector(
-      `#${tab.dataset.tabTarget}`
-    );
+//     //Remove Previous Activated Classes
+//     tabs.forEach((tab) => tab.classList.remove("active"));
+//     tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
 
-    //Remove Previous Activated Classes
-    tabs.forEach((tab) => tab.classList.remove("active"));
-    tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
+//     //Add New Active Classes
+//     tab.classList.add("active");
+//     targetTabContent.classList.add("active");
+//   });
+// });
 
-    //Add New Active Classes
-    tab.classList.add("active");
-    targetTabContent.classList.add("active");
-  });
+// Accordion
+const accordionHeader = document.querySelector(".accordion--header");
+const accordion = document.querySelector(".accordion");
+accordionHeader.addEventListener("click", (e) => {
+  accordion.classList.toggle("accordion--extended");
 });
