ddc595b8a1fe6d9932929fd1e37b7cfecf1cb2d0
Nima TorkZad <nimatorkzad@gmail.com>
2024-04-28 16:15:06 +0330

    radio added

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,31 @@
-// // Hamburger Menu :
+// Hamburger Menu :
 
-// const toggler = document.querySelector(".nav__toggler");
-// const navbar = document.querySelector(".nav");
-// toggler.addEventListener("click", (e) => {
-//   navbar.classList.toggle("nav__expanded");
-// });
+const toggler = document.querySelector(".nav__toggler");
+const navbar = document.querySelector(".nav");
+toggler.addEventListener("click", (e) => {
+  navbar.classList.toggle("nav__expanded");
+});
 
-// // Tapsi Services :
-// const tabs = document.querySelectorAll(".tab");
-// const tabContents = document.querySelectorAll(".tab-content");
-// tabs.forEach((tab) => {
-//   tab.addEventListener("click", (e) => {
-//     const targetTabContent = document.querySelector(
-//       `#${tab.dataset.tabTarget}`
-//     );
+// Tapsi Services :
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    const targetTabContent = document.querySelector(
+      `#${tab.dataset.tabTarget}`
+    );
 
-//     //Remove Previous Activated Classes
-//     tabs.forEach((tab) => tab.classList.remove("active"));
-//     tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
+    //Remove Previous Activated Classes
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
 
-//     //Add New Active Classes
-//     tab.classList.add("active");
-//     targetTabContent.classList.add("active");
-//   });
-// });
+    //Add New Active Classes
+    tab.classList.add("active");
+    targetTabContent.classList.add("active");
+  });
+});
 
 // Accordion
-// const accordionHeaders = document.querySelectorAll(".accordion--header");
 const accordion = document.querySelectorAll(".accordion");
 accordion.forEach((item) => {
   item.addEventListener("click", (e) => {
